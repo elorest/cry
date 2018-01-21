5f9040dbd7a5f7df900fef9d2afc819fb821e5e0
Davor Ocelic <docelic@spinlocksolutions.com>
2018-01-21 19:56:38 +0100

    Apply suggestions from PR #3

--- src/cry/command.cr
+++ src/cry/command.cr
@@ -3,17 +3,16 @@ require "colorize"
 
 module Cry
   class Command < ::Cli::Command
-    @filename : String
-    @result_filename : String
-    @filename = new_file
-    @result_filename = new_result_file
+
+    @filename_seed : Int64
+    @filename_seed = Time.now.epoch_ms
 
     class Options
       arg "code", desc: "Crystal code or .cr file to execute within the application scope", default: ""
       bool ["-l", "--log"], desc: "Prints results of previous runs"
       string ["-e", "--editor"], desc: "Prefered editor: [vim, nano, pico, etc], only used when no code or .cr file is specified", default: ENV["EDITOR"]? || "vim"
       string ["-b", "--back"], desc: "Runs previous command files: 'amber exec -b [times_ago]'", default: "0"
-      bool ["-o", "--loop"], desc: "Runs editor in a loop (can be combined with e.g. -b 1)"
+      bool ["-r", "--repeat"], desc: "Runs editor in a loop"
     end
 
     class Help
@@ -21,26 +20,25 @@ module Cry
     end
 
     def prepare_file
-      back_i = options.back.to_i(strict: false)
       _filename = if File.exists?(args.code)
                     args.code
-                  elsif back_i == 0
-                    @filename = new_file
-                  elsif back_i > 0
-                    Dir.glob("./tmp/*_console.cr").sort.reverse[back_i - 1]?
+                  elsif @back.not_nil! > 0
+                    Dir.glob("./tmp/*_console.cr").sort.reverse[@back.not_nil! - 1]?
                   end
 
-      system("cp #{_filename} #{@filename}") if _filename && File.exists?(_filename)
+      system("cp #{_filename} #{filename}") if _filename && File.exists?(_filename)
     end
 
-    def new_file
-      "./tmp/#{Time.now.epoch_ms}_console.cr"
+    def filename
+      "./tmp/#{@filename_seed}_console.cr"
     end
-    def new_result_file
-      @filename.sub("console.cr", "console_result.log")
+    def result_filename
+      "./tmp/#{@filename_seed}_console_result.log"
     end
 
     def run
+      @back = if args.repeat?; 1 else args.back.to_i(strict: false) || 0 end
+
       if args.log?
         logs = Dir.glob("./tmp/*_console_result.log")
         str = String.build do |s|
@@ -59,26 +57,26 @@ module Cry
 
         loop do
           unless args.code.blank? || File.exists?(args.code)
-            File.write(@filename, "puts (#{args.code}).inspect")
+            File.write(filename, "puts (#{args.code}).inspect")
           else
             prepare_file
-            system("#{options.editor} #{@filename}")
+            system("#{options.editor} #{filename}")
           end
 
-          break unless File.exists?(@filename)
+          break unless File.exists?(filename)
 
           result = ""
-          result = `crystal eval 'require "#{@filename}";'`
+          result = `crystal eval 'require "#{filename}";'`
 
-          File.write(@result_filename, result) unless result.nil?
+          File.write(result_filename, result) unless result.nil?
           puts result
 
-          break unless args.loop?
+          break unless args.repeat?
           puts "\nENTER to edit, q to quit"
           input = gets
           break if input=~ /^q/i
-          @filename = new_file
-          @result_filename = new_result_file
+
+          @filename_seed = Time.now.epoch_ms
         end
       end
     end
